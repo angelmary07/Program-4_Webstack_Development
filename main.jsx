e7bc393601396c067e53941f6d6028a433b2dd43
Angel Mary Sony <angelmary.sony@bcah.christuniversity.in>
2026-01-29 07:56:21 +0530

    Create  main.jsx

---
+++ main.jsx
@@ -0,0 +1,11 @@
+// src/index.js or main.jsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
